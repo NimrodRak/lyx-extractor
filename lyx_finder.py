690ce1af9bb922bbb3c649152dbfcd5ee8018c95
Nimrod Rak <nimrod.rak@mail.huji.ac.il>
2021-02-06 17:19:43 +0200

    finished fixing regex issue

--- lyx_finder.py
+++ lyx_finder.py
@@ -1,19 +1,23 @@
 import regex as re
 
 MACRO_PATTERN_STRING = rf"\\begin_inset FormulaMacro[\w\W]*?\\end_inset"
-THEOREM_PATTERN_STRING = r"\\begin_layout Description\n\b\{}\b[\w\W]*?\\end_layout".format("משפט")
+HEBREW_CLAIM = "טענה"
+HEBREW_THEOREM = "משפט"
+THEOREM_PATTERN_STRING = rf"\\begin_layout Description\n(?:(?:\b{HEBREW_CLAIM}\b)|(?:\b{HEBREW_THEOREM}\b))[\w\W]*?\\end_layout"
 HEADER_PATTERN_STRING = rf"^[\w\W]*?\\begin_body"
 BEGIN_STANDARD_LAYOUT = "\\begin_layout Standard\n"
 END_LAYOUT = "\\end_layout\n"
 END_DOCUMENT = "\\end_body\n\\end_document"
-NEW_FILE_NAME = "./new.lyx"
-RAW_FILE_NAME = "./raw.txt"
+NEW_FILE_NAME = "./lanew.lyx"
+RAW_FILE_NAME = "./latest.lyx"
 NEWLINE = "\n"
 
 def match_content(content, pattern_string):
     pattern = re.compile(pattern_string)
     matches = pattern.findall(content)
     return matches
+
+
 def main():
     with open(RAW_FILE_NAME, encoding="utf8") as content_file:
         content = content_file.read()
@@ -24,7 +28,7 @@ def main():
     header = headers[0]
     macros = match_content(content, MACRO_PATTERN_STRING)
     theorems = match_content(content, THEOREM_PATTERN_STRING)
-
+    
     with open(NEW_FILE_NAME, "w", encoding="utf8") as new_file:
         new_file.write(header + NEWLINE)
         new_file.write(BEGIN_STANDARD_LAYOUT)
@@ -32,7 +36,9 @@ def main():
             new_file.write(macro + NEWLINE)
         new_file.write(END_LAYOUT)
         for theorem in theorems:
-            new_file.write(theorem + NEWLINE)
+            BEGIN_LAYOUT_TEXT = "\\begin_layout Enumerate\n"
+            BEGIN_LAYOUT_LENGTH = len(r"\begin_layout Description משפט ")
+            new_file.write(BEGIN_LAYOUT_TEXT + theorem[BEGIN_LAYOUT_LENGTH:] + NEWLINE)
         new_file.write(END_DOCUMENT)
 
 if __name__ == "__main__":
