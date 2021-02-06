8d3db427247e392d022ce97e2affc528ca1e7c51
Nimrod Rak <nimrod.rak@mail.huji.ac.il>
2021-02-06 14:13:26 +0200

    refactored lyx_finder.py

--- lyx_finder.py
+++ lyx_finder.py
@@ -3,31 +3,37 @@ import regex as re
 MACRO_PATTERN_STRING = rf"\\begin_inset FormulaMacro[\w\W]*?\\end_inset"
 THEOREM_PATTERN_STRING = r"\\begin_layout Description\n\b\{}\b[\w\W]*?\\end_layout".format("משפט")
 HEADER_PATTERN_STRING = rf"^[\w\W]*?\\begin_body"
+BEGIN_STANDARD_LAYOUT = "\\begin_layout Standard\n"
+END_LAYOUT = "\\end_layout\n"
+END_DOCUMENT = "\\end_body\n\\end_document"
+NEW_FILE_NAME = "./new.lyx"
+RAW_FILE_NAME = "./raw.txt"
+NEWLINE = "\n"
 
+def match_content(content, pattern_string):
+    pattern = re.compile(pattern_string)
+    matches = pattern.findall(content)
+    return matches
 def main():
-    with open("./raw.txt", encoding="utf8") as content_file:
+    with open(RAW_FILE_NAME, encoding="utf8") as content_file:
         content = content_file.read()
-    
-    header_pattern = re.compile(HEADER_PATTERN_STRING)
-    theorem_pattern = re.compile(THEOREM_PATTERN_STRING)
-    macro_pattern = re.compile(MACRO_PATTERN_STRING)
-    matches = theorem_pattern.findall(content) 
-    header = header_pattern.search(content)
-    macros = macro_pattern.findall(content)
-
-    if header is None:
-        print("Error locating header! Exiting...")
+    headers = match_content(content, HEADER_PATTERN_STRING)
+    if len(headers) != 1:
+        print("Error locating proper header! Exiting...")
         return
+    header = headers[0]
+    macros = match_content(content, MACRO_PATTERN_STRING)
+    theorems = match_content(content, THEOREM_PATTERN_STRING)
 
-    with open("./new.lyx", "w", encoding="utf8") as new_file:
-        new_file.write(header.group() + "\n")
-        new_file.write("\\begin_layout Standard\n")
+    with open(NEW_FILE_NAME, "w", encoding="utf8") as new_file:
+        new_file.write(header + NEWLINE)
+        new_file.write(BEGIN_STANDARD_LAYOUT)
         for macro in macros:
-            new_file.write(macro + "\n")
-        new_file.write("\\end_layout\n")
-        for match in matches:
-            new_file.write(match + "\n")
-        new_file.write("\\end_body\n\\end_document")
+            new_file.write(macro + NEWLINE)
+        new_file.write(END_LAYOUT)
+        for theorem in theorems:
+            new_file.write(theorem + NEWLINE)
+        new_file.write(END_DOCUMENT)
 
 if __name__ == "__main__":
     main()
