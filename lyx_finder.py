fba4179cc819404782a6060094e3985c3418c84c
Nimrod Rak <nimrod.rak@mail.huji.ac.il>
2021-02-21 22:49:51 +0200

    generalized keywords

--- lyx_finder.py
+++ lyx_finder.py
@@ -3,17 +3,17 @@ import re
 MACRO_PATTERN_STRING = rf"\\begin_inset FormulaMacro[\w\W]*?\\end_inset"
 HEBREW_CLAIM = "טענה"
 HEBREW_THEOREM = "משפט"
-THEOREM_PATTERN_STRING = rf"\\begin_layout Description\n(?:(?:\b{HEBREW_CLAIM}\b)|(?:\b{HEBREW_THEOREM}\b))[\w\W]*?\\end_layout"
+HEBREW_COROLLARY = "מסקנה"
 HEADER_PATTERN_STRING = rf"^[\w\W]*?\\begin_body"
 BEGIN_STANDARD_LAYOUT = "\\begin_layout Standard\n"
 END_LAYOUT = "\\end_layout\n"
 BEGIN_LAYOUT_TEXT = "\\begin_layout Enumerate\n"
-BEGIN_LAYOUT_LENGTH = len(r"\begin_layout Description משפט ")
+BEGIN_LAYOUT_LENGTH = len(r"\begin_layout Description") + 1
 END_DOCUMENT = "\\end_body\n\\end_document"
 NEW_FILE_NAME = "./lanew.lyx"
 RAW_FILE_NAME = "./latest.lyx"
 NEWLINE = "\n"
-
+generate_key_concat = lambda lst: "(?:" + "|".join([rf"(?:\b{x}\b)" for x in lst]) + ")"
 def match_content(content, pattern_string):
     pattern = re.compile(pattern_string)
     matches = pattern.findall(content)
@@ -29,7 +29,9 @@ def main():
         return
     header = headers[0]
     macros = match_content(content, MACRO_PATTERN_STRING)
-    theorems = match_content(content, THEOREM_PATTERN_STRING)
+    keys_list = [HEBREW_CLAIM, HEBREW_COROLLARY, HEBREW_THEOREM]
+    theorem_pattern = rf"\\begin_layout Description\n{generate_key_concat(keys_list)}[\w\W]*?\\end_layout"
+    theorems = match_content(content, theorem_pattern)
     
     with open(NEW_FILE_NAME, "w", encoding="utf8") as new_file:
         new_file.write(header + NEWLINE)
@@ -38,7 +40,7 @@ def main():
             new_file.write(macro + NEWLINE)
         new_file.write(END_LAYOUT)
         for theorem in theorems:
-            new_file.write(BEGIN_LAYOUT_TEXT + theorem[BEGIN_LAYOUT_LENGTH:] + NEWLINE)
+            new_file.write(BEGIN_LAYOUT_TEXT + theorem[BEGIN_LAYOUT_LENGTH + theorem[BEGIN_LAYOUT_LENGTH:].find(" ") + 1:] + NEWLINE)
         new_file.write(END_DOCUMENT)
 
 
